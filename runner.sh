065a765c35947eb8640ab24bff24965fb08f2bbe
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2014-04-22 06:13:38 +0900

    WIP

--- runner.sh
+++ runner.sh
@@ -1,6 +1,6 @@
 # cronで定期的に初期化する
 
 #docker build -t fc2 .
-docker stop `docker ps -q`
-docker rm -v $(docker ps -a -q)
+docker stop $(docker ps -a | awk '/fc2:latest/ {print $1}')
+docker rm -v $(docker ps -a | awk '/fc2:latest/ {print $1}')
 docker run -d -p 80:80 fc2

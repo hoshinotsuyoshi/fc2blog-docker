39cc1ccb9ddc25a6807132a516fecf9c84a995e9
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2014-05-06 06:11:27 +0900

    Change runner.sh

--- runner.sh
+++ runner.sh
@@ -3,4 +3,4 @@
 #docker build -t fc2 .
 docker stop $(docker ps -a | awk '/fc2:latest/ {print $1}')
 docker rm -v $(docker ps -a | awk '/fc2:latest/ {print $1}')
-docker run -d -p 80:80 fc2
+docker run --name fc2blog -d -p 80 hoshinotsuyoshi/fc2
